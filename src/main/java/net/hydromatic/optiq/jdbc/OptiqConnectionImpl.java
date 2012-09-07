48fe1e7e36dad1931018cb545e9e6ce0c9d1e44e
Chris K Wensel <chris@wensel.net>
2012-09-07 15:47:02 -0700

    break out inner classes and make public

--- src/main/java/net/hydromatic/optiq/jdbc/OptiqConnectionImpl.java
+++ src/main/java/net/hydromatic/optiq/jdbc/OptiqConnectionImpl.java
@@ -17,34 +17,32 @@
 */
 package net.hydromatic.optiq.jdbc;
 
-import net.hydromatic.linq4j.*;
+import net.hydromatic.linq4j.BaseQueryable;
+import net.hydromatic.linq4j.Enumerator;
+import net.hydromatic.linq4j.QueryProvider;
+import net.hydromatic.linq4j.Queryable;
 import net.hydromatic.linq4j.expressions.Expression;
-
 import net.hydromatic.linq4j.expressions.Expressions;
 import net.hydromatic.linq4j.expressions.ParameterExpression;
 import net.hydromatic.optiq.DataContext;
 import net.hydromatic.optiq.MutableSchema;
 import net.hydromatic.optiq.impl.java.JavaTypeFactory;
 import net.hydromatic.optiq.impl.java.MapSchema;
-import net.hydromatic.optiq.runtime.ByteString;
 import net.hydromatic.optiq.server.OptiqServer;
 import net.hydromatic.optiq.server.OptiqServerStatement;
 
-import org.eigenbase.reltype.*;
-import org.eigenbase.sql.type.BasicSqlType;
-import org.eigenbase.sql.type.SqlTypeFactoryImpl;
-
-import java.lang.reflect.Field;
 import java.lang.reflect.Type;
-import java.math.BigDecimal;
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.Executor;
 
 /**
  * Implementation of JDBC connection
  * in the Optiq engine.
- *
+ * <p/>
  * <p>Abstract to allow newer versions of JDBC to add methods.</p>
  */
 abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
@@ -61,9 +59,9 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     private String catalog;
 
     final ParameterExpression rootExpression =
-        Expressions.parameter(DataContext.class, "root");
+            Expressions.parameter(DataContext.class, "root");
     final MutableSchema rootSchema =
-        new MapSchema(this, typeFactory, rootExpression);
+            new MapSchema(this, typeFactory, rootExpression);
     final UnregisteredDriver driver;
     final net.hydromatic.optiq.jdbc.Factory factory;
     private final String url;
@@ -74,7 +72,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
 
     final OptiqServer server = new OptiqServer() {
         final List<OptiqServerStatement> statementList =
-            new ArrayList<OptiqServerStatement>();
+                new ArrayList<OptiqServerStatement>();
 
         public void removeStatement(OptiqServerStatement optiqServerStatement) {
             statementList.add(optiqServerStatement);
@@ -87,20 +85,19 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
 
     /**
      * Creates an OptiqConnectionImpl.
-     *
+     * <p/>
      * <p>Not public; method is called only from the driver.</p>
      *
-     * @param driver Driver
+     * @param driver  Driver
      * @param factory Factory for JDBC objects
-     * @param url Server URL
-     * @param info Other connection properties
+     * @param url     Server URL
+     * @param info    Other connection properties
      */
     OptiqConnectionImpl(
-        UnregisteredDriver driver,
-        net.hydromatic.optiq.jdbc.Factory factory,
-        String url,
-        Properties info)
-    {
+            UnregisteredDriver driver,
+            net.hydromatic.optiq.jdbc.Factory factory,
+            String url,
+            Properties info) {
         this.driver = driver;
         this.factory = factory;
         this.url = url;
@@ -122,8 +119,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     // QueryProvider methods
 
     public <T> Queryable<T> createQuery(
-        Expression expression, Class<T> rowType)
-    {
+            Expression expression, Class<T> rowType) {
         return new OptiqQueryable<T>(this, rowType, expression);
     }
 
@@ -143,7 +139,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
         try {
             OptiqStatement statement = createStatement();
             OptiqPrepare.PrepareResult enumerable =
-                statement.prepare(queryable);
+                    statement.prepare(queryable);
             return (Enumerator<T>) enumerable.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -155,16 +151,16 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     public OptiqStatement createStatement() throws SQLException {
         //noinspection MagicConstant
         return createStatement(
-            ResultSet.TYPE_FORWARD_ONLY,
-            ResultSet.CONCUR_READ_ONLY,
-            holdability);
+                ResultSet.TYPE_FORWARD_ONLY,
+                ResultSet.CONCUR_READ_ONLY,
+                holdability);
     }
 
     public PreparedStatement prepareStatement(String sql) throws SQLException {
         //noinspection MagicConstant
         return prepareStatement(
-            sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY,
-            holdability);
+                sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY,
+                holdability);
     }
 
     public CallableStatement prepareCall(String sql) throws SQLException {
@@ -236,28 +232,25 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     }
 
     public Statement createStatement(
-        int resultSetType, int resultSetConcurrency) throws SQLException
-    {
+            int resultSetType, int resultSetConcurrency) throws SQLException {
         //noinspection MagicConstant
         return createStatement(
-            resultSetType, resultSetConcurrency, holdability);
+                resultSetType, resultSetConcurrency, holdability);
     }
 
     public PreparedStatement prepareStatement(
-        String sql,
-        int resultSetType,
-        int resultSetConcurrency) throws SQLException
-    {
+            String sql,
+            int resultSetType,
+            int resultSetConcurrency) throws SQLException {
         //noinspection MagicConstant
         return prepareStatement(
-            sql, resultSetType, resultSetConcurrency, holdability);
+                sql, resultSetType, resultSetConcurrency, holdability);
     }
 
     public CallableStatement prepareCall(
-        String sql,
-        int resultSetType,
-        int resultSetConcurrency) throws SQLException
-    {
+            String sql,
+            int resultSetType,
+            int resultSetConcurrency) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
@@ -271,8 +264,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
 
     public void setHoldability(int holdability) throws SQLException {
         if (!(holdability == ResultSet.CLOSE_CURSORS_AT_COMMIT
-              || holdability == ResultSet.HOLD_CURSORS_OVER_COMMIT))
-        {
+                || holdability == ResultSet.HOLD_CURSORS_OVER_COMMIT)) {
             throw new SQLException("invalid value");
         }
         this.holdability = holdability;
@@ -299,59 +291,53 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     }
 
     public OptiqStatement createStatement(
-        int resultSetType,
-        int resultSetConcurrency,
-        int resultSetHoldability) throws SQLException
-    {
+            int resultSetType,
+            int resultSetConcurrency,
+            int resultSetHoldability) throws SQLException {
         OptiqStatement statement =
-            factory.newStatement(
-                this, resultSetType, resultSetConcurrency,
-                resultSetHoldability);
+                factory.newStatement(
+                        this, resultSetType, resultSetConcurrency,
+                        resultSetHoldability);
         server.addStatement(statement);
         return statement;
     }
 
     public PreparedStatement prepareStatement(
-        String sql,
-        int resultSetType,
-        int resultSetConcurrency,
-        int resultSetHoldability) throws SQLException
-    {
+            String sql,
+            int resultSetType,
+            int resultSetConcurrency,
+            int resultSetHoldability) throws SQLException {
         OptiqPreparedStatement statement =
-            factory.newPreparedStatement(
-                this,
-                sql,
-                resultSetType,
-                resultSetConcurrency,
-                resultSetHoldability);
+                factory.newPreparedStatement(
+                        this,
+                        sql,
+                        resultSetType,
+                        resultSetConcurrency,
+                        resultSetHoldability);
         server.addStatement(statement);
         return statement;
     }
 
     public CallableStatement prepareCall(
-        String sql,
-        int resultSetType,
-        int resultSetConcurrency,
-        int resultSetHoldability) throws SQLException
-    {
+            String sql,
+            int resultSetType,
+            int resultSetConcurrency,
+            int resultSetHoldability) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
     public PreparedStatement prepareStatement(
-        String sql, int autoGeneratedKeys) throws SQLException
-    {
+            String sql, int autoGeneratedKeys) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
     public PreparedStatement prepareStatement(
-        String sql, int[] columnIndexes) throws SQLException
-    {
+            String sql, int[] columnIndexes) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
     public PreparedStatement prepareStatement(
-        String sql, String[] columnNames) throws SQLException
-    {
+            String sql, String[] columnNames) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
@@ -376,14 +362,12 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     }
 
     public void setClientInfo(
-        String name, String value) throws SQLClientInfoException
-    {
+            String name, String value) throws SQLClientInfoException {
         throw new UnsupportedOperationException();
     }
 
     public void setClientInfo(Properties properties)
-        throws SQLClientInfoException
-    {
+            throws SQLClientInfoException {
         throw new UnsupportedOperationException();
     }
 
@@ -396,14 +380,12 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     }
 
     public Array createArrayOf(
-        String typeName, Object[] elements) throws SQLException
-    {
+            String typeName, Object[] elements) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
     public Struct createStruct(
-        String typeName, Object[] attributes) throws SQLException
-    {
+            String typeName, Object[] attributes) throws SQLException {
         throw new UnsupportedOperationException();
     }
 
@@ -420,8 +402,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
     }
 
     public void setNetworkTimeout(
-        Executor executor, int milliseconds) throws SQLException
-    {
+            Executor executor, int milliseconds) throws SQLException {
         this.networkTimeout = milliseconds;
     }
 
@@ -434,7 +415,7 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
             return iface.cast(this);
         }
         throw helper.createException(
-            "does not implement '" + iface + "'");
+                "does not implement '" + iface + "'");
     }
 
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
@@ -445,114 +426,10 @@ abstract class OptiqConnectionImpl implements OptiqConnection, QueryProvider {
         return url.startsWith(CONNECT_STRING_PREFIX);
     }
 
-    private static class JavaTypeFactoryImpl
-        extends SqlTypeFactoryImpl
-        implements JavaTypeFactory
-    {
-        public RelDataType createStructType(Class type) {
-            List<RelDataTypeField> list = new ArrayList<RelDataTypeField>();
-            for (Field field : type.getFields()) {
-                // FIXME: watch out for recursion
-                list.add(
-                    new RelDataTypeFieldImpl(
-                        field.getName(),
-                        list.size(),
-                        createType(field.getType())));
-            }
-            return canonize(
-                new JavaRecordType(
-                    list.toArray(new RelDataTypeField[list.size()]),
-                    type));
-        }
-
-        public RelDataType createType(Type type) {
-            if (type instanceof RelDataType) {
-                return (RelDataType) type;
-            }
-            if (!(type instanceof Class)) {
-                throw new UnsupportedOperationException(
-                    "TODO: implement " + type + ": " + type.getClass());
-            }
-            final Class clazz = (Class) type;
-            if (clazz.isPrimitive()) {
-                return createJavaType(clazz);
-            } else if (clazz == String.class) {
-                // TODO: similar special treatment for BigDecimal, BigInteger,
-                //  Date, Time, Timestamp, Double etc.
-                return createJavaType(clazz);
-            } else if (clazz.isArray()) {
-                return createMultisetType(
-                    createType(clazz.getComponentType()), -1);
-            } else {
-                return createStructType(clazz);
-            }
-        }
-
-        public Type getJavaClass(RelDataType type) {
-            if (type instanceof RelRecordType) {
-                JavaRecordType javaRecordType;
-                if (type instanceof JavaRecordType) {
-                    javaRecordType = (JavaRecordType) type;
-                    return javaRecordType.clazz;
-                } else {
-                    return (RelRecordType) type;
-                }
-            }
-            if (type instanceof JavaType) {
-                JavaType javaType = (JavaType) type;
-                return javaType.getJavaClass();
-            }
-            if (type.isStruct() && type.getFieldCount() == 1) {
-                return getJavaClass(type.getFieldList().get(0).getType());
-            }
-            if (type instanceof BasicSqlType) {
-                switch (type.getSqlTypeName()) {
-                case VARCHAR:
-                case CHAR:
-                    return String.class;
-                case INTEGER:
-                    return Integer.class;
-                case BIGINT:
-                    return Long.class;
-                case SMALLINT:
-                    return Short.class;
-                case TINYINT:
-                    return Byte.class;
-                case DECIMAL:
-                    return BigDecimal.class;
-                case BOOLEAN:
-                    return Boolean.class;
-                case BINARY:
-                case VARBINARY:
-                    return ByteString.class;
-                case DATE:
-                    return java.sql.Date.class;
-                case TIME:
-                    return Time.class;
-                case TIMESTAMP:
-                    return Timestamp.class;
-                }
-            }
-            return null;
-        }
-    }
-
-    private static class JavaRecordType extends RelRecordType {
-        private final Class clazz;
-
-        public JavaRecordType(RelDataTypeField[] fields, Class clazz) {
-            super(fields);
-            this.clazz = clazz;
-            assert clazz != null;
-        }
-    }
-
     static class OptiqQueryable<T>
-        extends BaseQueryable<T>
-    {
+            extends BaseQueryable<T> {
         public OptiqQueryable(
-            OptiqConnection connection, Type elementType, Expression expression)
-        {
+                OptiqConnection connection, Type elementType, Expression expression) {
             super(connection, elementType, expression);
         }
 
